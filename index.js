532e2da4a5ff9f666591ff4716a64752e62b22df
Rajatsingh900k <rajatsingh900k@gmail.com>
2025-10-07 12:06:37 +0530

    Complete basics

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hello World")
+const express = require('express') //require module syntax
+// or import express from 'express'
+const app = express();
+const port = 3000
+
+// here app is listening 'Get' request on home('/') route.
+app.get('/', (req, res) => {
+  res.send('Hello World!').send //in response we will send() reponse
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send('twitter.com')
+})
+
+app.get('/login',(req,res)=>{
+    res.send('<h1>Please logIn</h1>')
+})
+
+app.get('/chai',(req,res)=>{
+    res.send('Chai aur code')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
